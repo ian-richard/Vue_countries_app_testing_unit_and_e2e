eb161b65a1b639798a50fe7341fe0834bf8d949b
Ian F <ian.fiddeman@gmail.com>
2020-11-27 14:00:49 +0000

    countries app with removing from favs button added

---
+++ testing_start/src/components/FavouriteListItem.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div id="favourite_countries">
+    <h2>Favourite Countries</h2>
+    <ul>
+      <li v-for="(favouriteCountry, index) in favouriteCountries">{{favouriteCountry.name}} 
+        <img class="small-flag" :src="favouriteCountry.flag"/>
+      <button v-on:click="handleDelete(index)">Remove from favs</button></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import {eventBus} from '@/main.js';
+
+export default {
+  name: 'favourite-countries',
+  props: ['favouriteCountries'],
+  methods: {
+    handleDelete: function(index) {
+      eventBus.$emit('country-deleted', index);
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
